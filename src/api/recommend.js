dd47b621808ec74990628352611655027bd1d16a
MelodicTechno <617179025@qq.com>
2025-03-10 20:36:54 +0800

    修正vite.config.js

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -4,7 +4,7 @@ import request from '@/utils/request';
 const API_URL = '/api'; // 根据你的 Vite 配置，这里使用 /api 作为基础路径
 
 export const sendLocationToBackendService = (lat, lng) => {
-    return request.post(`/statistic/nearby/`, {
+    return request.post(API_URL + `/businesses/nearby/`, {
         latitude: lat,
         longitude: lng
     });
